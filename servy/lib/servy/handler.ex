72d0b598cafcd6eea5b2f43d1b15d122d506ab8e
dubesoftware <therealdubesoftware@gmail.com>
2022-12-24 17:11:54 +0200

    Since we define a response in Servy.Handler.format_response, remove redundant expected_response variable outside the module

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -32,14 +32,6 @@ Accept: */*
 
 """
 
-expected_response = """
-HTTP/1.1 200 OK
-Content-Type: text/html
-Content-Length: 20
-
-Bears, Lions, Tigers
-"""
-
 response = Servy.Handler.handle(request)
 
 IO.puts response
