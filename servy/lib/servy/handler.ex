1b7868ad7ffdb014e1ae1b4e14580a166f8e9186
dubesoftware <therealdubesoftware@gmail.com>
2023-01-02 17:57:40 +0200

    Implement parse function

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -7,9 +7,12 @@ defmodule Servy.Handler do
   end
 
   def parse(request) do
-    first_line = request |> String.split("\n") |> List.first
-    parts = String.split(first_line, " ")
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "" }
+    [method, path, _] =
+      request
+        |> String.split("\n")
+        |> List.first
+        |> String.split(" ")
+    %{ method: method, path: path, resp_body: "" }
   end
 
   def route(conv) do
