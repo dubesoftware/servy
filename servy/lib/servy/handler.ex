0740db325b62c17f8ebf5d1f3b62cc2b92aee27a
dubesoftware <therealdubesoftware@gmail.com>
2023-01-05 21:45:20 +0200

    Add method parameter to route function clauses

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -19,14 +19,14 @@ defmodule Servy.Handler do
   end
 
   def route(conv) do
-    route(conv, conv.path)
+    route(conv, conv.method, conv.path)
   end
 
-  def route(conv, "/wildthings") do
+  def route(conv, "GET", "/wildthings") do
     %{ conv | resp_body: "Bears, Lions, Tigers" }
   end
 
-  def route(conv, "/bears") do
+  def route(conv, "GET", "/bears") do
     %{ conv | resp_body: "Teddy, Smokey, Paddington" }
   end
 
