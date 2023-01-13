0e4a3c0294b8ee192efc6c176936874b0ce495bd
dubesoftware <therealdubesoftware@gmail.com>
2023-01-13 19:26:48 +0200

    Update route function clause matching /about path to read and return file content

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -82,6 +82,15 @@ defmodule Servy.Handler do
   end
 
   def route(%{ method: "GET", path: "/about" } = conv) do
+    case File.read("../../about.html") do
+      {:ok, content} ->
+        %{ conv | status: 200, resp_body: content }
+      {:error, :enoent} ->
+        %{ conv | status: 404, resp_body: "File not found!" }
+      {:error, reason} ->
+        %{ conv | status: 500, resp_body: "File error: #{reason}" }
+    end
+    
     %{ conv | status: 200, resp_body: "contents of file" }
   end
 
