25d317cf772c15b18a20cf571e592572592bf6a8
dubesoftware <therealdubesoftware@gmail.com>
2023-01-15 21:54:23 +0200

    Update function clause matching /bears/new path to use case statement

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -82,10 +82,18 @@ defmodule Servy.Handler do
   end
 
   def route(%{ method: "GET", path: "/bears/new" } = conv) do
-    Path.expand("../../pages", __DIR__)
+    file =
+      Path.expand("../../pages", __DIR__)
       |> Path.join("form.html")
-      |> File.read
-      |> handle_file(conv)
+    
+    case File.read(file) do
+      {:ok, content} ->
+        %{ conv | status: 200, resp_body: content }
+      {:error, :enoent} ->
+        %{ conv | status: 404, resp_body: "File not found!" }
+      {:error, reason} ->
+        %{ conv | status: 500, resp_body: "File error: #{reason}" }
+    end
   end
 
   def route(%{ method: "GET", path: "/about" } = conv) do
