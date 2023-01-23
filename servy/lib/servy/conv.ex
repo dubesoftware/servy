e19da52bdd6051a04f1b0b00d3a4bbe51af692fc
dubesoftware <therealdubesoftware@gmail.com>
2023-01-23 03:16:41 +0200

    Define and call Conv.full_status function

--- servy/lib/servy/conv.ex
+++ servy/lib/servy/conv.ex
@@ -1,3 +1,7 @@
 defmodule Servy.Conv do
   defstruct method: "", path: "", resp_body: "", status: nil
+
+  def full_status(conv) do
+    "#{conv.status} #{status_reason(conv.status)}"
+  end
 end
--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -79,7 +79,7 @@ defmodule Servy.Handler do
 
   def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
