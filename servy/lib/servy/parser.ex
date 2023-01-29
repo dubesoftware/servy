31f49bcf1d9124dc12991f4c224046f50881f91b
dubesoftware <therealdubesoftware@gmail.com>
2023-01-29 18:07:01 +0200

    Define parse_headers function

--- servy/lib/servy/parser.ex
+++ servy/lib/servy/parser.ex
@@ -18,6 +18,10 @@ defmodule Servy.Parser do
     }
   end
 
+  def parse_headers([head | tail]) do
+    parse_headers(tail)
+  end
+
   def parse_params(params_string) do
     params_string |> String.trim |> URI.decode_query
   end
