8194e37829328d860a155f64c5aa49ff464ec870
dubesoftware <therealdubesoftware@gmail.com>
2023-01-30 00:34:42 +0200

    Add tripled function clauses example

--- servy/lib/servy/recurse.ex
+++ servy/lib/servy/recurse.ex
@@ -6,6 +6,16 @@ defmodule Servy.Recurse do
   end
 
   def sum([], total), do: total
+
+  def triple([head | tail], tripled) do
+    IO.puts "Head: #{head} Tail: #{inspect(tail)}"
+    tripled = [tripled, head * 3]
+    triple(tail, tripled)
+  end
+
+  def triple([], tripled), do: List.flatten(tripled)
 end
   
 IO.inspect Servy.Recurse.sum([1, 2, 3, 4, 5], 0)
+
+IO.inspect Servy.Recurse.triple([1, 2, 3, 4, 5], [])
