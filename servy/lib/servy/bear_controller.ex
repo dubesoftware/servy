fe3d9d9c64476297ac81c1e377895cefa3dd238e
dubesoftware <therealdubesoftware@gmail.com>
2023-01-31 19:15:24 +0200

    Pass id parameter to show function

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -3,7 +3,7 @@ defmodule Servy.BearController do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
-  def show(conv) do
+  def show(conv, id) do
     %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
 end
