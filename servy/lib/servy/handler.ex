d768fb61e1055b97d8daa25309e1f717fe49082c
dubesoftware <therealdubesoftware@gmail.com>
2023-02-04 01:37:49 +0200

    Remove optional request with content type of multipart/form-data

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -236,18 +236,3 @@ name=Baloo&type=Brown
 response = Servy.Handler.handle(request)
 
 IO.puts response
-
-request = """
-POST /bears HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-Content-Type: multipart/form-data
-Content-Length: 21
-
-name=Baloo&type=Brown
-"""
-
-response = Servy.Handler.handle(request)
-
-IO.puts response
