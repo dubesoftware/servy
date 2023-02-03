b28535c6d7774eeb8dfff3894428277f00d86ebc
dubesoftware <therealdubesoftware@gmail.com>
2023-02-04 01:59:42 +0200

    Add get_bear function clause to handle instances when an ID is binary

--- servy/lib/servy/wildthings.ex
+++ servy/lib/servy/wildthings.ex
@@ -20,4 +20,8 @@ defmodule Servy.Wildthings do
   def get_bear(id) when is_integer(id) do
     Enum.find(fn(b) -> b.id == id end)
   end
+
+  def get_bear(id) when is_binary(id) do
+    id |> String.to_integer |> get_bear
+  end
 end
