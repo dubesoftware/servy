4865e5f343718d67cdea4eb5a5b350d0bd1acd88
dubesoftware <therealdubesoftware@gmail.com>
2023-02-04 21:26:36 +0200

    Remove bear item generation

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -13,7 +13,6 @@ defmodule Servy.BearController do
     items =
       Wildthings.list_bears()
       |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
       |> Enum.join 
 
     %{ conv | status: 200, resp_body: "<ul>#{items}</ul>" }
