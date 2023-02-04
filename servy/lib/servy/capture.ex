e79def4f6a846080ed4b0e4c63e4c8ff6acf3850
dubesoftware <therealdubesoftware@gmail.com>
2023-02-04 03:59:22 +0200

    Capture addition expression

--- servy/lib/servy/capture.ex
+++ servy/lib/servy/capture.ex
@@ -2,6 +2,11 @@ defmodule Servy.Capture do
   def add do
     fn(a, b) -> a + b end
   end
+
+  def shorthand_add do
+    &(&1 + &2)
+  end
 end
 
 IO.inspect Servy.Capture.add.(1, 2)
+IO.inspect Servy.Capture.shorthand_add.(3, 4)
