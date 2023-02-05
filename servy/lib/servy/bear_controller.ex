5b779a2fc53c101e6bc9f050b8111d076b8586e6
dubesoftware <therealdubesoftware@gmail.com>
2023-02-05 19:29:33 +0200

    Use list of bears to generate template content

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -6,11 +6,16 @@ defmodule Servy.BearController do
   @templates_path Path.expand("../../templates", File.cwd!)
 
   def index(conv) do
-    items =
+    bears =
       Wildthings.list_bears()
       |> Enum.sort(&Bear.order_asc_by_name/2)
 
-    %{ conv | status: 200, resp_body: "<ul>#{items}</ul>" }
+      content =
+        @templates_path
+        |> Path.join("index.eex")
+        |> EEx.eval_file(bears: bears)
+
+    %{ conv | status: 200, resp_body: content }
   end
 
   def show(conv, %{"id" => id}) do
