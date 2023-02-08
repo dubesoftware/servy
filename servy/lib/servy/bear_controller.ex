b717eb657df585cc817bdc0061cc72cebfd514ca
dubesoftware <therealdubesoftware@gmail.com>
2023-02-08 18:17:02 +0200

    Update bear_controller.ex as per code sample

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -3,14 +3,14 @@ defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
 
-  import Servy.View, only: [render: 3]
+  @templates_path Path.expand("../../templates", __DIR__)
 
   def index(conv) do
     bears =
       Wildthings.list_bears()
       |> Enum.sort(&Bear.order_asc_by_name/2)
 
-      render(conv, "index.eex", bears: bears)
+    render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
@@ -19,11 +19,18 @@ defmodule Servy.BearController do
     render(conv, "show.eex", bear: bear)
   end
 
-  def create(conv, %{"name" => name, "type" => type} = params) do
-    %{ conv | status: 201, resp_body: "Created a #{type} bear named #{name}!" }
+  def create(conv, %{"name" => name, "type" => type}) do
+    %{ conv | status: 201,
+              resp_body: "Created a #{type} bear named #{name}!" }
   end
 
-  def delete(conv, _params) do
-    %{ conv | status: 403, resp_body: "Bears must never be deleted!"}
+  defp render(conv, template, bindings \\ []) do
+    content =
+      @templates_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+
+    %{ conv | status: 200, resp_body: content }
   end
+
 end
