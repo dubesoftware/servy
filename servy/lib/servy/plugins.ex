035467004f32a16d66638bcfa1c2c8af83a46c51
dubesoftware <therealdubesoftware@gmail.com>
2023-02-08 18:19:53 +0200

    Update plugins.ex as per code sample

--- servy/lib/servy/plugins.ex
+++ servy/lib/servy/plugins.ex
@@ -1,36 +1,27 @@
 defmodule Servy.Plugins do
-  import Logger
 
   alias Servy.Conv
 
-  @doc "Logs 404 requests."
+  @doc "Logs 404 requests"
   def track(%Conv{status: 404, path: path} = conv) do
-    IO.puts "Warning: #{path} is on the loose!"
+    if Mix.env != :test do
+      IO.puts "Warning: #{path} is on the loose!"
+    end
     conv
   end
 
   def track(%Conv{} = conv), do: conv
 
-  def log(%Conv{} = conv) do
-    Logger.info "Inspecting conversation map..."
-    IO.inspect conv
-  end
-
   def rewrite_path(%Conv{path: "/wildlife"} = conv) do
     %{ conv | path: "/wildthings" }
   end
 
-  def rewrite_path(%Conv{path: path} = conv) do
-    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
-    captures = Regex.named_captures(regex, path)
-    rewrite_path_captures(conv, captures)
-  end
-
   def rewrite_path(%Conv{} = conv), do: conv
 
-  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
-    %{ conv | path: "/#{thing}/#{id}" }
+  def log(%Conv{} = conv) do
+    if Mix.env == :dev do
+      IO.inspect conv
+    end
+    conv
   end
-
-  def rewrite_path_captures(conv, nil), do: conv
 end
