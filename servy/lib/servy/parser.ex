02f950dcf92107e89c95d764ae68e3a974b0e979
dubesoftware <therealdubesoftware@gmail.com>
2023-02-08 18:20:47 +0200

    Update parser.ex as per code sample

--- servy/lib/servy/parser.ex
+++ servy/lib/servy/parser.ex
@@ -4,18 +4,21 @@ defmodule Servy.Parser do
 
   def parse(request) do
     [top, params_string] = String.split(request, "\r\n\r\n")
+
     [request_line | header_lines] = String.split(top, "\r\n")
+
     [method, path, _] = String.split(request_line, " ")
 
     headers = parse_headers(header_lines, %{})
+
     params = parse_params(headers["Content-Type"], params_string)
 
     %Conv{
-      method: method,
-      path: path,
-      params: params,
-      headers: headers
-    }
+       method: method,
+       path: path,
+       params: params,
+       headers: headers
+     }
   end
 
   def parse_headers([head | tail], headers) do
@@ -23,15 +26,8 @@ defmodule Servy.Parser do
     headers = Map.put(headers, key, value)
     parse_headers(tail, headers)
   end
-  
-  def parse_headers([], headers), do: headers
 
-  # def parse_headers(header_lines) do
-  #   Enum.reduce(header_lines, %{}, fn(line, headers_so_far) ->
-  #     [key, value] = String.split(line, ": ")
-  #     Map.put(headers_so_far, key, value)
-  #   end)
-  # end
+  def parse_headers([], headers), do: headers
 
   @doc """
   Parses the given param string of the form `key1=value1&key2=value2`
@@ -49,4 +45,5 @@ defmodule Servy.Parser do
   end
 
   def parse_params(_, _), do: %{}
+
 end
