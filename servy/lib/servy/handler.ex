a0f0cc81d66d1935fdb722b2c04653e10e554c68
dubesoftware <therealdubesoftware@gmail.com>
2023-02-08 18:15:55 +0200

    Update handler.ex as per code sample

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -1,15 +1,15 @@
 defmodule Servy.Handler do
-	@moduledoc "Handles HTTP requests."
+
+  @moduledoc "Handles HTTP requests."
 
   alias Servy.Conv
   alias Servy.BearController
 
-  @pages_path Path.expand("../../pages", File.cwd!)
+  @pages_path Path.expand("../../pages", __DIR__)
 
-  import Servy.FileHandler, only: [handle_file: 2]
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
-  
+
   @doc "Transforms the request into a response."
   def handle(request) do
     request
@@ -21,13 +21,6 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def route(%Conv{ method: "GET", path: "/pages/" <> file } =  conv) do
-    @pages_path
-    |> Path.join(file <> ".html")
-    |> File.read
-    |> handle_file(conv)
-  end
-
   def route(%Conv{ method: "GET", path: "/wildthings" } = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
@@ -36,59 +29,46 @@ defmodule Servy.Handler do
     BearController.index(conv)
   end
 
-  def route(%Conv{ method: "POST", path: "/bears" } = conv) do
-    BearController.create(conv, conv.params)
-  end
-
-  def route(%Conv{ method: "GET", path: "/bears/new" } = conv) do
-    file = Path.join(@pages_path, "form.html")
-    
-    case File.read(file) do
-      {:ok, content} ->
-        %{ conv | status: 200, resp_body: content }
-      {:error, :enoent} ->
-        %{ conv | status: 404, resp_body: "File not found!" }
-      {:error, reason} ->
-        %{ conv | status: 500, resp_body: "File error: #{reason}" }
-    end
-  end
-
-  def route(%Conv{ method: "DELETE", path: "/bears/" <> _id } = conv) do
-    BearController.delete(conv, conv.params)
-  end
-
   def route(%Conv{ method: "GET", path: "/bears/" <> id } = conv) do
     params = Map.put(conv.params, "id", id)
     BearController.show(conv, params)
   end
 
-  def route(%Conv{ method: "GET", path: "/about" } = conv) do
-    @pages_path
+  def route(%Conv{method: "POST", path: "/bears"} = conv) do
+    BearController.create(conv, conv.params)
+  end
+
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
+      @pages_path
       |> Path.join("about.html")
       |> File.read
       |> handle_file(conv)
   end
 
   def route(%Conv{ path: path } = conv) do
-    %{ conv | status: 404, resp_body: "No #{path} here!" }
+    %{ conv | status: 404, resp_body: "No #{path} here!"}
   end
 
-  def emojify(%{status: 200} = conv) do
-    emojies = String.duplicate("🎉", 5)
-    body = emojies <> "\n" <> conv.resp_body <> "\n" <> emojies
-    
-    %{ conv | resp_body: body }
+  def handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content }
   end
 
-  def emojify(conv), do: conv
+  def handle_file({:error, :enoent}, conv) do
+    %{ conv | status: 404, resp_body: "File not found!" }
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File error: #{reason}" }
+  end
 
   def format_response(%Conv{} = conv) do
     """
     HTTP/1.1 #{Conv.full_status(conv)}\r
     Content-Type: text/html\r
-    Content-Length: #{byte_size(conv.resp_body)}\r
+    Content-Length: #{String.length(conv.resp_body)}\r
     \r
     #{conv.resp_body}
     """
   end
+
 end
