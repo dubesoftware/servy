36e85269fb5debf8772a6476c2867e736d5a967c
dubesoftware <therealdubesoftware@gmail.com>
2023-02-09 03:22:44 +0200

    Define Servy.Api.BearController.index action

--- servy/lib/servy/api/bear_controller.ex
+++ servy/lib/servy/api/bear_controller.ex
@@ -1,3 +1,11 @@
 defmodule Servy.Api.BearController do
   
+  def index(conv) do
+    json =
+      Servy.Wildthings.list_bears()
+      |> Poison.encode!
+    
+    %{ conv | status: 200, resp_body: json }
+  end
+
 end
