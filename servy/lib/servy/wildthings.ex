3de1c7954d7556ffd9939aee6af5e31972b4943a
dubesoftware <therealdubesoftware@gmail.com>
2023-02-10 20:41:25 +0200

    Define read_json private function

--- servy/lib/servy/wildthings.ex
+++ servy/lib/servy/wildthings.ex
@@ -11,6 +11,16 @@ defmodule Servy.Wildthings do
     |> Map.get("bears")
   end
 
+  defp read_json(source) do
+    case File.read(source) do
+      {:ok, contents} ->
+        contents
+      {:error, reason} ->
+        IO.inspect "Error reading #{source}: #{reason}"
+        "[]"
+    end
+  end
+
   def get_bear(id) when is_integer(id) do
     Enum.find(list_bears(), fn(b) -> b.id == id end)
   end
