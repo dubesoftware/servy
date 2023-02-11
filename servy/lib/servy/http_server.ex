0d7134e3a34624054a355f20532158424f6bc05c
dubesoftware <therealdubesoftware@gmail.com>
2023-02-11 23:22:14 +0200

    Create http_server.ex file

---
+++ servy/lib/servy/http_server.ex
@@ -0,0 +1,8 @@
+server() ->
+    {ok, LSock} = gen_tcp:listen(5678, [binary, {packet, 0}, 
+                                        {active, false}]),
+    {ok, Sock} = gen_tcp:accept(LSock),
+    {ok, Bin} = do_recv(Sock, []),
+    ok = gen_tcp:close(Sock),
+    ok = gen_tcp:close(LSock),
+    Bin.
