9c8bcda8eb6187eeea5ef58d45ef7395c65a1ec2
dubesoftware <therealdubesoftware@gmail.com>
2023-02-14 00:26:55 +0200

    Revert "Implement Servy.Timer.reminder function"
    This reverts commit 854f2c8406df8f4b55e230f605aa07ec37bcdbdb.

--- servy/lib/servy/timer.ex
+++ servy/lib/servy/timer.ex
@@ -1,11 +1,3 @@
 defmodule Servy.Timer do
   
-  def remind(reminder, seconds_in_future) do
-    seconds_in_future
-      |> String.to_integer
-      |> :timer.sleep
-    
-    IO.puts reminder
-  end
-
 end
