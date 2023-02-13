854f2c8406df8f4b55e230f605aa07ec37bcdbdb
dubesoftware <therealdubesoftware@gmail.com>
2023-02-14 00:19:48 +0200

    Implement Servy.Timer.reminder function

--- servy/lib/servy/timer.ex
+++ servy/lib/servy/timer.ex
@@ -1,3 +1,11 @@
 defmodule Servy.Timer do
   
+  def remind(reminder, seconds_in_future) do
+    seconds_in_future
+      |> String.to_integer
+      |> :timer.sleep
+    
+    IO.puts reminder
+  end
+
 end
