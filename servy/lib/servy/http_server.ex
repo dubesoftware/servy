3ce859cd7859630f9aab16e1521cc2bb9f61be00
dubesoftware <therealdubesoftware@gmail.com>
2023-02-13 19:06:14 +0200

    Print ID of current process

--- servy/lib/servy/http_server.ex
+++ servy/lib/servy/http_server.ex
@@ -45,6 +45,7 @@ defmodule Servy.HttpServer do
   sends a response back over the same socket.
   """
   def serve(client_socket) do
+    IO.puts "#{self()}"
     client_socket
     |> read_request
     |> Servy.Handler.handle
