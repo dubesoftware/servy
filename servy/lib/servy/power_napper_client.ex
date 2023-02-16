c67f258de277464d710378faec2f8ccf1efad8de
dubesoftware <therealdubesoftware@gmail.com>
2023-02-16 23:22:10 +0200

    Simulate power nap call

--- servy/lib/servy/power_napper_client.ex
+++ servy/lib/servy/power_napper_client.ex
@@ -1,3 +1,10 @@
 defmodule Servy.PowerNapperClient do
-  
+  alias Servy.PowerNapper
+
+  parent = self()
+  spawn(fn -> send(parent, {:slept, PowerNapper.power_nap.()}) end)
+
+  receive do
+    {:slept, time} -> IO.puts "Slept #{time} ms"
+  end
 end
