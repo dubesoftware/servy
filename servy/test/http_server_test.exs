06d26d4d5d4a65ecdba9e7624d6237409211d1d5
dubesoftware <therealdubesoftware@gmail.com>
2023-02-17 06:05:34 +0200

    Revert "User HTTPoison instead of HttpClient to send requests to the HttpServer module and assert the responses"
    This reverts commit 7d9f0d2a3b3c0e03a7db6e9195c0e92f4177e635.

--- servy/test/http_server_test.exs
+++ servy/test/http_server_test.exs
@@ -2,13 +2,27 @@ defmodule HttpServerTest do
   use ExUnit.Case
 
   alias Servy.HttpServer
+  alias Servy.HttpClient
 
   test "accepts a request on a socket and sends back a response" do
     spawn(HttpServer, :start, [4000])
 
-    {:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
+    request = """
+    GET /wildthings HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
 
-    assert response.status_code == 200
-    assert response.body == "Bears, Lions, Tigers"
+    response = HttpClient.send_request(request)
+
+    assert response == """
+    HTTP/1.1 200 OK\r
+    Content-Type: text/html\r
+    Content-Length: 20\r
+    \r
+    Bears, Lions, Tigers
+    """
   end
 end
