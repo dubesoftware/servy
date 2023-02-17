d8a8e0a75ab1991e8f3889032284180de948010c
dubesoftware <therealdubesoftware@gmail.com>
2023-02-17 18:24:23 +0200

    Fix async function definition

--- servy/lib/servy/fetcher.ex
+++ servy/lib/servy/fetcher.ex
@@ -1,5 +1,5 @@
 defmodule Servy.Fetcher do
-  def asyc(camera_name) do
+  def async(camera_name) do
     parent = self()
 
     spawn(fn -> send(parent, {:result, Servy.VideoCam.get_snapshot(camera_name)}) end)
