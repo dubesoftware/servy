e08730f9eeec4dd5aec57e48b43c3e5066887bd1
dubesoftware <therealdubesoftware@gmail.com>
2023-02-20 22:54:00 +0200

    Call create_pledge/2 and recent_pledges/0 functions from Servy.PledgeServer module

--- servy/lib/servy/pledge_controller.ex
+++ servy/lib/servy/pledge_controller.ex
@@ -1,14 +1,14 @@
 defmodule Servy.PledgeController do  
   def create(conv, %{"name" => name, "amount" => amount}) do
     # Send the pledge to the external service and caches it
-    create_pledge(name, String.to_integer(amount))
+    Servy.PledgeServer.create_pledge(name, String.to_integer(amount))
 
     %{ conv | status: 201, resp_body: "#{name} pledged #{amount}!" }
   end
 
   def index(conv) do
     # Gets the recent pledges form the cache
-    pledges = recent_pledges()
+    pledges = Servy.PledgeServer.recent_pledges()
 
     %{ conv | status: 200, resp_body: (inspect pledges) }
   end
