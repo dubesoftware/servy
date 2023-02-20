ae278e28693baa2cd34561c2c937399ec62b652b
dubesoftware <therealdubesoftware@gmail.com>
2023-02-20 22:33:47 +0200

    Define Servy.PledgeController.create function

--- servy/lib/servy/pledge_controller.ex
+++ servy/lib/servy/pledge_controller.ex
@@ -1,3 +1,10 @@
 defmodule Servy.PledgeController do
   
+  def create(conv, %{"name" => name, "amount" => amount}) do
+    # Send the pledge to the external service and caches it
+    create_pledge(name, String.to_integer(amount))
+
+    %{ conv | status: 201, resp_body: "#{name} pledged #{amount}!" }
+  end
+
 end
