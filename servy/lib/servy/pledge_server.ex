e68dfcd8b1bcd2ab5e3dc20646600f336a68f662
dubesoftware <therealdubesoftware@gmail.com>
2023-02-20 22:56:41 +0200

    Define create_pledge/2 and recent_pledges/0 functions

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -1,3 +1,9 @@
 defmodule Servy.PledgeServer do
-  
+  def create_pledge(name, amount) do
+    # Create pledge here
+  end
+
+  def recent_pledges() do
+    # Return recent pledges from cache here
+  end
 end
