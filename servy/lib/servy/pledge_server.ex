f2c684188c351d7a72e2b8730730c8ab95446db2
dubesoftware <therealdubesoftware@gmail.com>
2023-02-23 03:17:16 +0200

    Simulate caching the pledge

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -1,6 +1,9 @@
 defmodule Servy.PledgeServer do
   def create_pledge(name, amount) do
     %{:ok, id} = send_pledge_to_service(name, amount)
+		
+		# Cache the pledge
+		[ {"larry", 10} ]
   end
 
   defp send_pledge_to_service(_name, _amount) do
