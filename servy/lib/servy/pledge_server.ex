1b53a48fcb9689d9df3b8d349ccc0a573da21792
dubesoftware <therealdubesoftware@gmail.com>
2023-02-23 15:59:09 +0200

    Comment out create_pledge and recent_pledges methods

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -1,15 +1,15 @@
 defmodule Servy.PledgeServer do
-  def create_pledge(name, amount) do
-    {:ok, id} = send_pledge_to_service(name, amount)
-		
-		# Cache the pledge:
-		[ {"larry", 10} ]
-  end
-	
-	def recent_pledges do
-		# Returns the most recent pledges (cache):
-		[ {"larry", 10} ]
-	end
+	#   def create_pledge(name, amount) do
+	#     {:ok, id} = send_pledge_to_service(name, amount)
+	#
+	# 	# Cache the pledge:
+	# 	[ {"larry", 10} ]
+	#   end
+	#
+	# def recent_pledges do
+	# 	# Returns the most recent pledges (cache):
+	# 	[ {"larry", 10} ]
+	# end
 
   defp send_pledge_to_service(_name, _amount) do
     # Send pledge to external service:
