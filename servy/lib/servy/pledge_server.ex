a537812f0741fc656d568caaea9a9613387b5149
dubesoftware <therealdubesoftware@gmail.com>
2023-02-24 00:00:20 +0200

    Implement listen_loop listening function

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -1,4 +1,14 @@
 defmodule Servy.PledgeServer do
+	def listen_loop do
+		IO.puts "\nWaiting for a message..."
+		
+		receive do
+			{:create_pledge, name, amount} ->
+				{:ok, id} = send_pledge_to_service(name, amount)
+				IO.puts "#{name} pledged #{amount}!"
+				listen_loop()
+		end
+	end
 	#   def create_pledge(name, amount) do
 	#     {:ok, id} = send_pledge_to_service(name, amount)
 	#
