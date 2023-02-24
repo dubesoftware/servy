fcf24a3892b8d5a2629a568915be1c055ccd38d3
dubesoftware <therealdubesoftware@gmail.com>
2023-02-24 04:28:50 +0200

    Revert "Display total pledges"
    This reverts commit 1ae9ff67a11b4aa9ad669bc157e3b33f23f3bd7d.

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -56,5 +56,3 @@ IO.inspect PledgeServer.create_pledge("daisy", 40)
 IO.inspect PledgeServer.create_pledge("grace", 50)
 
 IO.inspect PledgeServer.recent_pledges()
-
-IO.inspect PledgeServer.total_pledged()
