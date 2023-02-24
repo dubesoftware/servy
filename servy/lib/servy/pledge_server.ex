79e4f799877d8fa5a7d4c9fabbe9eda2ee38bce5
dubesoftware <therealdubesoftware@gmail.com>
2023-02-24 21:21:06 +0200

    Add call to PledgeServer.clear/1 function

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -72,6 +72,9 @@ IO.inspect PledgeServer.create_pledge("larry", 10)
 IO.inspect PledgeServer.create_pledge("moe", 20)
 IO.inspect PledgeServer.create_pledge("curly", 30)
 IO.inspect PledgeServer.create_pledge("daisy", 40)
+
+PledgeServer.clear()
+
 IO.inspect PledgeServer.create_pledge("grace", 50)
 
 IO.inspect PledgeServer.recent_pledges()
