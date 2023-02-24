f9e004fffe0a7dff11b7cd84afba704486d22e82
dubesoftware <therealdubesoftware@gmail.com>
2023-02-24 03:08:31 +0200

    In lieu of hard-coded module name, pass __MODULE__ to PledgeServer.start/0 function

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -1,7 +1,7 @@
 defmodule Servy.PledgeServer do
 	def start do
 		IO.puts "Starting the pledge server..."
-		pid = spawn(Servy.PledgeServer, :listen_loop, [[]])
+		pid = spawn(__MODULE__, :listen_loop, [[]])
 	end
 	
   def listen_loop(state) do
