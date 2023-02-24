547c2e77ad603acca5006084eb3221dd6ed7a72e
dubesoftware <therealdubesoftware@gmail.com>
2023-02-24 23:59:02 +0200

    Move the listen_loop and the other generic code to a GenericServer module

--- servy/lib/servy/four_oh_four_counter.ex
+++ servy/lib/servy/four_oh_four_counter.ex
@@ -1,33 +1,9 @@
-defmodule Servy.FourOhFourCounter do
-
-  @name :four_oh_four_counter
-
-  # Client Interface
-
-  def start do
-    IO.puts "Starting the 404 counter..."
-    pid = spawn(__MODULE__, :listen_loop, [%{}])
-    Process.register(pid, @name)
+defmodule Servy.GenericServer do
+  def start(callback_module, initial_state, name) do
+    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
+    Process.register(pid, name)
     pid
   end
-
-  def bump_count(path) do
-    call @name, {:bump_count, path}
-  end
-
-  def get_counts do
-    call @name, :get_counts
-  end
-
-  def get_count(path) do
-    call @name, {:get_count, path}
-  end
-
-  def reset do
-    cast @name, :reset
-  end
-
-  # Helper Functions
 
   def call(pid, message) do
     send pid, {:call, self(), message}
@@ -39,22 +15,52 @@ defmodule Servy.FourOhFourCounter do
     send pid, {:cast, message}
   end
 
-  # Server
-
-  def listen_loop(state) do
+  def listen_loop(state, callback_module) do
     receive do
       {:call, sender, message} when is_pid(sender) ->
-        {response, new_state} = handle_call(message, state)
+        {response, new_state} = callback_module.handle_call(message, state)
         send sender, {:response, response}
-        listen_loop(new_state)
+        listen_loop(new_state, callback_module)
       {:cast, message} ->
-        new_state = handle_cast(message, state)
-        listen_loop(new_state)
+        new_state = callback_module.handle_cast(message, state)
+        listen_loop(new_state, callback_module)
       unexpected ->
         IO.puts "Unexpected messaged: #{inspect unexpected}"
-        listen_loop(state)
+        listen_loop(state, callback_module)
     end
   end
+end
+
+defmodule Servy.FourOhFourCounter do
+
+  @name :four_oh_four_counter
+
+  alias Servy.GenericServer
+
+  # Client Interface
+
+  def start do
+    IO.puts "Starting the 404 counter..."
+    GenericServer.start(__MODULE__, %{}, @name)
+  end
+
+  def bump_count(path) do
+    GenericServer.call @name, {:bump_count, path}
+  end
+
+  def get_counts do
+    GenericServer.call @name, :get_counts
+  end
+
+  def get_count(path) do
+    GenericServer.call @name, {:get_count, path}
+  end
+
+  def reset do
+    GenericServer.cast @name, :reset
+  end
+
+  # Server Callbacks
 
   def handle_call({:bump_count, path}, state) do
     new_state = Map.update(state, path, 1, &(&1 + 1))
