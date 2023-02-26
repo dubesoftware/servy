52cd3c07c9030adc6992618028c4b4946a8be916
dubesoftware <therealdubesoftware@gmail.com>
2023-02-26 21:29:34 +0200

    Add :reply atom to return tuples

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -35,18 +35,18 @@ defmodule Servy.PledgeServer do
 	
 	def handle_call(:total_pledged, state) do
 		total = Enum.map(state, &elem(&1, 1)) |> Enum.sum
-		{total, state}
+		{:reply, total, state}
 	end
 	
 	def handle_call(:recent_pledges, state) do
-		{state, state}
+		{:reply, state, state}
 	end
 	
 	def handle_call({:create_pledge, name, amount}, state) do
     {:ok, id} = send_pledge_to_service(name, amount)
 		most_recent_pledges = Enum.take(state, 2)
     new_state = [{name, amount} | most_recent_pledges]
-		{id, new_state}
+		{:reply, id, new_state}
 	end
 
   defp send_pledge_to_service(_name, _amount) do
