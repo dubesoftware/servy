f346f4028f84f90269456621b8bc87b5be544727
dubesoftware <therealdubesoftware@gmail.com>
2023-02-27 19:24:34 +0200

    Define get_sensor_data/0 function

--- servy/lib/servy/sensor_server.ex
+++ servy/lib/servy/sensor_server.ex
@@ -9,6 +9,10 @@ defmodule Servy.SensorServer do
   def start do
     GenServer.start(__MODULE__, %{}, name: @name)
   end
+  
+  def get_sensor_data do
+    GenServer.call @name, get_sensor_data
+  end
 
   # Server Callbacks
 
