73520796f61e1de399892f860362f61c71495b5b
dubesoftware <therealdubesoftware@gmail.com>
2023-02-27 14:55:32 +0200

    Pass _from as unused second argument to callback functions

--- servy/lib/servy/four_oh_four_counter.ex
+++ servy/lib/servy/four_oh_four_counter.ex
@@ -29,16 +29,16 @@ defmodule Servy.FourOhFourCounter do
 
   # Server Callbacks
 
-  def handle_call({:bump_count, path}, state) do
+  def handle_call({:bump_count, path}, _from, state) do
     new_state = Map.update(state, path, 1, &(&1 + 1))
     {:ok, new_state}
   end
 
-  def handle_call(:get_counts, state) do
+  def handle_call(:get_counts, _from, state) do
     {state, state}
   end
 
-  def handle_call({:get_count, path}, state) do
+  def handle_call({:get_count, path}, _from, state) do
     count = Map.get(state, path, 0)
     {count, state}
   end
