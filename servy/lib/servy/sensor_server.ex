005bae79831cce2a98569f7fde1c69310a87c304
dubesoftware <therealdubesoftware@gmail.com>
2023-02-28 14:54:35 +0200

    Define run_tasks_to_get_sensor_data/0 function

--- servy/lib/servy/sensor_server.ex
+++ servy/lib/servy/sensor_server.ex
@@ -25,4 +25,19 @@ defmodule Servy.SensorServer do
     {:reply, state, state}
   end
 
+  defp run_tasks_to_get_sensor_data do
+    IO.puts "Running tasks to get sensor data..."
+
+    task = Task.async(fn -> Servy.Tracker.get_location("bigfoot") end)
+
+    snapshots =
+      ["cam-1", "cam-2", "cam-3"]
+      |> Enum.map(&Task.async(fn -> Servy.VideoCam.get_snapshot(&1) end))
+      |> Enum.map(&Task.await/1)
+    
+    where_is_bigfoot = Task.await(task)
+
+    %{snapshots: snapshots, location: where_is_bigfoot}
+  end
+
 end
