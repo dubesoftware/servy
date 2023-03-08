d577a6b43c275ae2db6615fc6b3cfaf1bcb82600
dubesoftware <therealdubesoftware@gmail.com>
2023-03-08 17:42:31 +0200

    Update source to mirror lesson sample

--- servy/lib/servy/sensor_server.ex
+++ servy/lib/servy/sensor_server.ex
@@ -1,4 +1,5 @@
-defmodule Servy.SensorServer do  
+defmodule Servy.SensorServer do
+
   @name :sensor_server
 
   use GenServer
@@ -8,9 +9,9 @@ defmodule Servy.SensorServer do
   def start do
     GenServer.start(__MODULE__, %{}, name: @name)
   end
-  
+
   def get_sensor_data do
-    GenServer.call @name, get_sensor_data
+    GenServer.call @name, :get_sensor_data
   end
 
   # Server Callbacks
@@ -33,7 +34,7 @@ defmodule Servy.SensorServer do
       ["cam-1", "cam-2", "cam-3"]
       |> Enum.map(&Task.async(fn -> Servy.VideoCam.get_snapshot(&1) end))
       |> Enum.map(&Task.await/1)
-    
+
     where_is_bigfoot = Task.await(task)
 
     %{snapshots: snapshots, location: where_is_bigfoot}
