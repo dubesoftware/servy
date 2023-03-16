6ec1caee3d92e130b7d66cec0a0502858133a2f8
dubesoftware <therealdubesoftware@gmail.com>
2023-03-16 16:57:34 +0200

    Call 'schedule_refresh' private function to schedule next refresh message for 5 seconds into the future

--- servy/lib/servy/sensor_server.ex
+++ servy/lib/servy/sensor_server.ex
@@ -18,14 +18,14 @@ defmodule Servy.SensorServer do
 
   def init(_state) do
     initial_state = run_tasks_to_get_sensor_data()
-    Process.send_after(self(), :refresh, :timer.seconds(5))
+    schedule_refresh()
     {:ok, initial_state}
   end
 
   def handle_info(:refresh, _state) do
     IO.puts "Refreshing the cache..."
     new_state = run_tasks_to_get_sensor_data()
-    Process.send_after(self(), :refresh, :timer.seconds(5))
+    schedule_refresh()
     {:noreply, new_state}
   end
 
