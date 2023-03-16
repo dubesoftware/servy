37eb332a25d261eb507fece4335325b14fafce9d
dubesoftware <therealdubesoftware@gmail.com>
2023-03-16 16:50:23 +0200

    Define handle info function clause

--- servy/lib/servy/sensor_server.ex
+++ servy/lib/servy/sensor_server.ex
@@ -22,6 +22,11 @@ defmodule Servy.SensorServer do
     {:ok, initial_state}
   end
 
+  def handle_info(:refresh, _state) do
+    IO.puts "Refreshing the cache..."
+    run_tasks_to_get_sensor_data()
+  end
+
   def handle_call(:get_sensor_data, _from, state) do
     {:reply, state, state}
   end
