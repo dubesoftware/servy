8351cce76a9d7d92d958eb90908d6a1f555cc712
dubesoftware <therealdubesoftware@gmail.com>
2023-03-21 21:18:29 +0200

    Define start_link/0 function

--- servy/lib/servy/services_supervisor.ex
+++ servy/lib/servy/services_supervisor.ex
@@ -1,3 +1,8 @@
 defmodule Servy.ServicesSupervisor do
   use Supervisor
+
+  def start_link do
+    IO.puts "Starting the services supervisor..."
+    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
 end
