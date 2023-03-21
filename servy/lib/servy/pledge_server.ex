49d8e4fa7dcf7e5f1bf8b11dd1a8949b01df2def
dubesoftware <therealdubesoftware@gmail.com>
2023-03-21 23:42:47 +0200

    Comment out sample code

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -88,23 +88,23 @@ defmodule Servy.PledgeServer do
 	end
 end
 
-alias Servy.PledgeServer
+# alias Servy.PledgeServer
 
-{:ok, pid} = PledgeServer.start()
+# {:ok, pid} = PledgeServer.start()
 
-send pid, {:stop, "hammertime"}
+# send pid, {:stop, "hammertime"}
 
-PledgeServer.set_cache_size(4)
+# PledgeServer.set_cache_size(4)
 
-IO.inspect PledgeServer.create_pledge("larry", 10)
+# IO.inspect PledgeServer.create_pledge("larry", 10)
 
-# PledgeServer.clear()
+# # PledgeServer.clear()
 
-# IO.inspect PledgeServer.create_pledge("moe", 20)
-# IO.inspect PledgeServer.create_pledge("curly", 30)
-# IO.inspect PledgeServer.create_pledge("daisy", 40)
-# IO.inspect PledgeServer.create_pledge("grace", 50)
+# # IO.inspect PledgeServer.create_pledge("moe", 20)
+# # IO.inspect PledgeServer.create_pledge("curly", 30)
+# # IO.inspect PledgeServer.create_pledge("daisy", 40)
+# # IO.inspect PledgeServer.create_pledge("grace", 50)
 
-IO.inspect PledgeServer.recent_pledges()
+# IO.inspect PledgeServer.recent_pledges()
 
-IO.inspect PledgeServer.total_pledged()
+# IO.inspect PledgeServer.total_pledged()
