8c689d8344af2be2133b7ecd5d40d63ce95aaba7
dubesoftware <therealdubesoftware@gmail.com>
2023-03-21 23:29:02 +0200

    Revert "Rename start function to start_link"
    This reverts commit 24b517bc47ebec3b08d27e3ab84288fe453636ce.

--- servy/lib/servy/sensor_server.ex
+++ servy/lib/servy/sensor_server.ex
@@ -11,7 +11,7 @@ defmodule Servy.SensorServer do
 
   # Client Interface
 
-  def start_link do
+  def start do
     IO.puts "Starting the sensor server..."
     GenServer.start(__MODULE__, %State{}, name: @name)
   end
