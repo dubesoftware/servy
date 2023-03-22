cd17f9e0969fe4a2b983248dbbe5bfdaf9511fdd
dubesoftware <therealdubesoftware@gmail.com>
2023-03-23 00:54:38 +0200

    Revert "Use the refresh interval passed to the start_link function to change the interval used by the process"
    This reverts commit 4a023ab86ed05a211da5faa76fa70d00b36e139d.

--- servy/lib/servy/sensor_server.ex
+++ servy/lib/servy/sensor_server.ex
@@ -14,8 +14,7 @@ defmodule Servy.SensorServer do
 
   def start_link(interval) do
     IO.puts "Starting the sensor server with #{interval} min refresh..."
-    initial_state = %State{refresh_interval: interval}
-    GenServer.start_link(__MODULE__, initial_state, name: @name)
+    GenServer.start_link(__MODULE__, %{}, name: @name)
   end
 
   def get_sensor_data do
