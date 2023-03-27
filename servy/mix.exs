c1c6a0adb7e241ba4e3960304902237120287062
dubesoftware <therealdubesoftware@gmail.com>
2023-03-27 22:35:13 +0200

    Revert "Specify module to call during application start-up"
    This reverts commit 38e691ba2491056a5a0cd4fbaf8e9b01ddadb668.

--- servy/mix.exs
+++ servy/mix.exs
@@ -15,8 +15,7 @@ defmodule Servy.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :eex],
-      mod: {Servy, []}
+      extra_applications: [:logger, :eex]
     ]
   end
 
